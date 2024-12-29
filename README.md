4085fd462edb03e2ecd6439c052272062ad3585c
A Kot <fiend69@yandex.ru>
2024-12-29 23:00:14 +0300

    My tests

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
+
 # qa_python
+
+##Список реализованных тестов
+
+1. Проверяем добавление новой книги без жанра
+    def test_add_new_book_add_book_no_genre(self, collector):
+    
+2. Проверяем, что книга с названием длиной более 40 символов не добавляется в коллекцию
+    def test_add_new_book_len_name_more_40(self, collector):
+ 
+3. Проверяем, что книга с названием длиной 0 символов не добавляется в коллекцию
+    def test_add_new_book_len_name_0(self, collector):
+
+4. Проверяем добавление жанра книге
+    def test_set_book_genre_new_add_new_genre(self, collector):
+        
+5. Проверяем вывод жанра книги по её имени
+    def test_get_book_genre_get_genre(self, collector):
+        
+6. Проверяем, что метод возвращает книги с указанным жанром
+    def test_get_books_with_specific_genre_get_books_with_new_genre(self, collector):
+        
+7. Проверяем метод вывода текущего словаря
+    def test_get_books_genre_get_current_dictionaryv(self, collector):
+        
+8. Проверяем, что книги без возрастных ограничений - подходят детям
+    def test_get_books_for_children_not_age_limit(self, collector):
+        
+9. Проверяем, что книги с возрастным ограничением - не подходят детям
+    def test_get_books_for_children_with_age_limit(self, collector):
+        
+10. Проверяем, что книга добавлена в избранное
+    def test_add_book_in_favorites_add_favorites_book(self, collector):
+      
+11. Проверяем, что книга удалена из избранного
+    def test_delete_book_from_favorites_delete_favorites_book(self, collector):
+      
+12. Проверяем, получение списка книг, добавленных в избранное
+    def test_get_list_of_favorites_books_list_books(self, collector):
+        
--- tests.py
+++ tests.py
@@ -1,3 +1,6 @@
+import random
+import pytest
+
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -18,7 +21,89 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+        # напиши свои тесты ниже
+        # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+
+    def test_add_new_book_add_book_no_genre(self, collector):
+        collector.add_new_book("teстирование dot COM")
+        assert collector.books_genre["teстирование dot COM"] == ""
+
+
+    def test_add_new_book_len_name_more_40(self, collector):
+        collector.add_new_book('q' * 41)
+        assert 'q' not in collector.books_genre
+
+
+    def test_add_new_book_len_name_0(self, collector):
+        collector.add_new_book('')
+        assert '' not in collector.books_genre
+
+
+    def test_set_book_genre_new_add_new_genre(self, collector):
+        collector.add_new_book('Пикник на обочине')
+        collector.set_book_genre('Пикник на обочине', 'Фантастика')
+        assert collector.get_book_genre('Пикник на обочине') == 'Фантастика'
+
+
+    def test_get_book_genre_get_genre(self, collector):
+        collector.add_new_book('Молчание ягнят')
+        collector.set_book_genre('Молчание ягнят', 'Ужасы')
+        genre = collector.get_book_genre('Молчание ягнят')
+        assert genre == 'Ужасы'
+
+    def test_get_books_with_specific_genre_get_books_with_new_genre(self, collector):
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Молчание ягнят')
+        collector.set_book_genre('Молчание ягнят', 'Ужасы')
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+        assert sorted(collector.get_books_with_specific_genre('Ужасы')) == sorted(
+            ['Молчание ягнят', 'Гордость и предубеждение и зомби'])
+
+
+    def test_get_books_genre_get_current_dictionaryv(self, collector):
+        collector.add_new_book('teстирование dot COM')
+        collector.add_new_book('Пикник на обочине')
+        collector.add_new_book('Молчание ягнят')
+        collector.set_book_genre('Пикник на обочине', 'Фантастика')
+        collector.set_book_genre('Молчание ягнят', 'Ужасы')
+        result = collector.get_books_genre()
+        books = {
+            'teстирование dot COM': '',
+            'Пикник на обочине': 'Фантастика',
+            'Молчание ягнят': 'Ужасы'
+        }
+        assert books == result
+
+
+    def test_get_books_for_children_not_age_limit(self, collector):
+        collector.add_new_book('Голубой щенок')
+        collector.set_book_genre('Голубой щенок', 'Мультфильмы')
+        assert 'Голубой щенок' in collector.get_books_for_children()
+
+
+    def test_get_books_for_children_with_age_limit(self, collector):
+        collector.add_new_book('Молчание ягнят')
+        collector.set_book_genre('Молчание ягнят', 'Ужасы')
+        assert 'Молчание ягнят' not in collector.get_books_for_children()
+
+
+    def test_add_book_in_favorites_add_favorites_book(self, collector):
+        collector.add_new_book('Солёный пёс')
+        collector.add_book_in_favorites('Солёный пёс')
+        assert 'Солёный пёс' in collector.get_list_of_favorites_books()
+
+
+    def test_delete_book_from_favorites_delete_favorites_book(self, collector):
+        collector.add_new_book('Солёный пёс')
+        collector.add_book_in_favorites('Солёный пёс')
+        collector.delete_book_from_favorites('Солёный пёс')
+        assert 'Солёный пёс' not in collector.favorites
+
+
+    def test_get_list_of_favorites_books_list_books(self, collector):
+        collector.add_new_book('Бегемот')
+        collector.add_book_in_favorites('Бегемот')
+        assert collector.get_list_of_favorites_books()
